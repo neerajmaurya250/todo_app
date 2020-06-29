6e4dcc1cb1d8fbf4cd62fd017ede178c516fdb6e
Neeraj Kumar Maurya <36784272+n-k-maurya@users.noreply.github.com>
2020-06-29 22:38:34 +0530

    todo

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,45 +1,47 @@
-//import 'package:cloud_firestore/cloud_firestore.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
-//import 'package:todoapp/models/users.dart';
-//
-//class AuthService {
-//
-//  final FirebaseAuth _auth = FirebaseAuth.instance;
-//
-//  // create user obj based on firebase user
-//  User _userFromFirebaseUser(FirebaseUser user) {
-//    return user != null ? User(uid: user.uid) : null;
-//  }
-//
-//  // auth change user stream
-//  Stream<User> get user {
-//    return _auth.onAuthStateChanged
-//    //.map((FirebaseUser user) => _userFromFirebaseUser(user));
-//        .map(_userFromFirebaseUser);
-//  }
-//
-//
-//  // register with email and password
-//  Future registerWithEmailAndPassword(String email, String password,String name, String empId) async {
-//    try {
-//      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-//      FirebaseUser user = result.user;
-//      Firestore.instance.collection('users').document(user.email).setData({"name": name, "email":email, "empId":empId});
-//      return _userFromFirebaseUser(user);
-//    } catch (error) {
-//      print(error.toString());
-//      return null;
-//    }
-//  }
-//
-//  // sign out
-//  Future signOut() async {
-//    try {
-//      return await _auth.signOut();
-//    } catch (error) {
-//      print(error.toString());
-//      return null;
-//    }
-//  }
-//
-//}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todoapp/models/users.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user obj based on firebase user
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+    //.map((FirebaseUser user) => _userFromFirebaseUser(user));
+        .map(_userFromFirebaseUser);
+  }
+
+
+  // register with email and password
+  Future registerWithEmailAndPassword(String email, String password,String name, String empId) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+      Firestore.instance.collection('users').document(user.email).setData({"name": name, "email":email, "empId":empId});
+      return _userFromFirebaseUser(user);
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+
+}
+
+
