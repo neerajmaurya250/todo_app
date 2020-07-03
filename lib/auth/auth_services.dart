3e885ae4038d4b200c1a47d5a29b791f387ed44f
Neeraj Kumar Maurya <36784272+n-k-maurya@users.noreply.github.com>
2020-07-03 21:48:22 +0530

    todo

--- lib/auth/auth_services.dart
+++ lib/auth/auth_services.dart
@@ -1,8 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todoapp/models/users.dart';
 
 class AuthServices {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user obj based on firebase user
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+    //.map((FirebaseUser user) => _userFromFirebaseUser(user));
+        .map(_userFromFirebaseUser);
+  }
+
 
   Future registerWithEmailAndPassword(
       String email,
